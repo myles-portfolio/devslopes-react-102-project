5ee29f57f300c707a761d3736bacc7e2c933272b
mylesh <84793716+myles-portfolio@users.noreply.github.com>
2023-06-03 20:37:46 -0500

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import "@/css/App.css";
+import { LogIn } from "@/components/LogIn";
+
+function App() {
+	return (
+		<>
+			<div>
+				<h1>Code Commerce</h1>
+				<LogIn />
+			</div>
+		</>
+	);
+}
+
+export default App;
---
+++ src/components/LogIn.tsx
@@ -0,0 +1,20 @@
+export const LogIn = () => {
+	return (
+		<form>
+			<label htmlFor="email">Email Address</label>
+			<input
+				type="email"
+				placeholder="emailaddress@domain.com"
+				id="email"
+				name="email"
+			/>
+			<label htmlFor="password">Password</label>
+			<input
+				type="email"
+				placeholder="emailaddress@domain.com"
+				id="email"
+				name="email"
+			/>
+		</form>
+	);
+};
